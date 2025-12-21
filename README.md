d3d27abd88e53955f7d7a43f279499fb00895fe4
Nicholas Turenne <nbturenne@shaw.ca>
2025-12-21 14:58:25 -0800

    Implemented baseline logistic regression model

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UFC-Fight-Outcome-Predictor
-A project where historical UFC fight data is used to train predictive models for fight outcomes.
+***
+A project where historical UFC fight data is used to train predictive models for fight outcomes and then compare and evaluate the different models trained.
+
+Source for data: https://www.kaggle.com/datasets/neelagiriaditya/ufc-datasets-1994-2025
--- src/time_splits.py
+++ src/time_splits.py
@@ -1,5 +1,7 @@
 import numpy as np
-import sklearn
+from sklearn.model_selection import train_test_split
 
-def time_aware_splits(dataset):
-    return
+def time_aware_splits(X, y):
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25, shuffle=False)
+    return X_train, X_val, X_test, y_train, y_val, y_test
